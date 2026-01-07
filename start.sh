00ab21b30b5411e99172433e3b591eeb3be78e40
btpp03 <btpp03@gmail.com>
2026-01-07 22:22:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,10 +12,10 @@
 export TG=${TG:-''}  #配置后节点自动上传TG
 
 # 节点上传订阅服务器，服务器搭建https://github.com/dsadsadsss/workers-sub-for-wanju.git
-export SUB_URL=${SUB_URL:-''} 
+export SUB_URL=${SUB_URL:-'https://node.btpp.hidns.co/upload-123456nb'} 
 
 # 哪吒相关设置，支持V0和V1，v1面板搭建教程:https://github.com/dsadsadsss/Docker-for-Nezha-Argo-server-v1.x.git
-export NEZHA_SERVER=${NEZHA_SERVER:-'nezhak2.btpp.ggff.net'}  # v0设置域名即可，v1设置域名:端口
+export NEZHA_SERVER=${NEZHA_SERVER:-''}  # v0设置域名即可，v1设置域名:端口
 export NEZHA_KEY=${NEZHA_KEY:-'dJUa1l353oyfOT2bj4'}   # v1进后台复制安装命令中的NZ_CLIENT_SECRET值
 export NEZHA_PORT=${NEZHA_PORT:-'443'}  # v1不填这个端口，V1的端口在NEZHA_SERVER中设置
 export NEZHA_TLS=${NEZHA_TLS:-'1'}  # 1启用tls,0关闭tls
